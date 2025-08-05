1a45d5f1aed3a87f9518c2e951a56509741bbd25
BryantV <Velasquezbrys@gmail.com>
2025-08-05 19:10:41 -0400

    feat: added login functionality

--- client/src/components/login-form.tsx
+++ client/src/components/login-form.tsx
@@ -1,18 +1,24 @@
-import { cn } from "@/lib/utils"
-import { Button } from "@/components/ui/button"
-import {
-  Card,
-  CardContent,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
-
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { supabase } from "../supabaseClient";
+import React, { useState } from "react";
 export function LoginForm({
   className,
   ...props
 }: React.ComponentProps<"div">) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function signIn() {
+    const { data, error } = await supabase.auth.signInWithPassword({email, password});
+    console.log(error)
+    console.log(data)
+  }
+
+
   return (
     <div className={cn("flex flex-col gap-6", className)} {...props}>
       <Card>
@@ -20,7 +26,7 @@ export function LoginForm({
           <CardTitle className="text-xl">Welcome back</CardTitle>
         </CardHeader>
         <CardContent>
-          <form>
+          <form >
             <div className="grid gap-6">
               <div className="flex flex-col gap-4">
                 <Button variant="outline" className="w-full">
@@ -44,8 +50,10 @@ export function LoginForm({
                   <Input
                     id="email"
                     type="email"
+                    value={email}
                     placeholder="example@example.com"
                     required
+                    onChange={(e) => setEmail(e.target.value)}
                   />
                 </div>
                 <div className="grid gap-3">
@@ -58,11 +66,19 @@ export function LoginForm({
                       Forgot your password?
                     </a> */}
                   </div>
-                  <Input id="password" type="password" required />
+                  <Input
+                    id="password"
+                    type="password"
+                    required
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
                 </div>
-                <Button type="submit" className="w-full">
+               
+                <Button type="submit" className="w-full" onClick={signIn}>
                   Login
                 </Button>
+                
               </div>
               <div className="text-center text-sm">
                 Don&apos;t have an account?{" "}
@@ -75,5 +91,5 @@ export function LoginForm({
         </CardContent>
       </Card>
     </div>
-  )
+  );
 }
